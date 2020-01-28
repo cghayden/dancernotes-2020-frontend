9f7a482d8b0b18abdf3897eb89e92b6d567a9115
Corey <cghayden@gmail.com>
2020-01-28 09:45:34 -0500

    test change for git

--- components/themeVariables.js
+++ components/themeVariables.js
@@ -88,6 +88,7 @@ const theme = {
   background: colors["gray1"],
   border: colors["vividBlue4"],
 
+  testChangeForGit: "testing123",
   sidebarWidth: "18vw",
   controlPanelWidth: "22vw",
   black: "#393939",
