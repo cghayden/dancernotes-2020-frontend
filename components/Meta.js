3ece83ec9cf6d62a2738d347b415d77ecb7eb13e
Corey Hayden <cghayden@users.noreply.github.com>
2020-02-07 21:35:40 -0500

    Update Meta.js

--- components/Meta.js
+++ components/Meta.js
@@ -4,7 +4,6 @@ const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
     <link rel="shortcut icon" href="/dnIcon.png" />
     <link rel="apple-touch-icon" href="/dnIcon.png" />
     <link rel="stylesheet" href="https://use.typekit.net/gkh6amt.css" />
