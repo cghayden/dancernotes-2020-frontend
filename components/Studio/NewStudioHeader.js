77499a426895c6f96aba4ce7ad8258aeba887827
Corey <cghayden@gmail.com>
2020-10-30 15:49:51 -0400

    changed color of header

--- components/Studio/NewStudioHeader.js
+++ components/Studio/NewStudioHeader.js
@@ -2,15 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 const Header = styled.header`
-  /* position: fixed;
-  left: 0;
-  right: 0;
-  top: 0; */
-  background: ${(props) => props.theme.black};
+  background: ${(props) => props.theme.gray5};
   display: flex;
   align-items: center;
   height: ${(props) => props.theme.studioHeaderHeight};
   padding: 0 20px;
+  color: white;
   .search {
     border-radius: 5px;
     background: ${(props) => props.theme.gray0};
--- components/Studio/NewStudioLayout.js
+++ components/Studio/NewStudioLayout.js
@@ -3,7 +3,7 @@ import NewStudioHeader from "./NewStudioHeader";
 import NewStudioNav from "./NewStudioNav";
 // import NewPageOptions from "./NewPageOptions";
 // import NewStudioMain from "./NewStudioMain";
-import { useStudio } from "../../components/Studio/useStudio";
+// import { useStudio } from "../../components/Studio/useStudio";
 
 const BodyLayout = styled.div`
   display: grid;
@@ -25,14 +25,13 @@ const NewStudioMain = styled.div`
 `;
 
 export default function NewStudioLayout({ children }) {
-  const studio = useStudio();
+  // const studio = useStudio();
 
   return (
     <Layout>
       <NewStudioHeader />
-      <BodyLayout className="bodyLayout">
+      <BodyLayout>
         <NewStudioNav />
-        {/* subnav on every page */}
         <NewStudioMain>{children}</NewStudioMain>
       </BodyLayout>
     </Layout>
