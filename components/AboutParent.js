55b3b342cfff603933eb0f47e5350aa6833e0acd
Corey Hayden <cghayden@gmail.com>
2021-12-07 11:23:40 -0500

    updated demo video link

--- components/AboutParent.js
+++ components/AboutParent.js
@@ -1,13 +1,13 @@
-import { AboutUl } from './styles/AboutStyles'
-import styled from 'styled-components'
-import Card from './styles/Card'
-import ParentDemoVideo from './Parent/ParentVideo'
+import { AboutUl } from './styles/AboutStyles';
+import styled from 'styled-components';
+import Card from './styles/Card';
+import ParentDemoVideo from './Parent/ParentVideo';
 
 const PageSignInStyle = styled.div`
   height: 50px;
   display: grid;
   place-items: center;
-`
+`;
 const MessageDiv = styled.div`
   /* width: 90%; */
   text-align: center;
@@ -17,13 +17,13 @@ const MessageDiv = styled.div`
     margin: 0px 1rem 0 1rem;
     color: ${(props) => props.theme.indigo8};
   }
-`
+`;
 
 const messages = [
   `All of your dancers' information, from all of their studios, in one
 place`,
   `Keep your own dance notes if a studio is not using Dancer Notes`,
-]
+];
 
 const AboutParent = ({ setView }) => {
   return (
@@ -36,7 +36,7 @@ const AboutParent = ({ setView }) => {
       </MessageDiv>
       <ParentDemoVideo
         src={
-          'https://res.cloudinary.com/coreytesting/video/upload/v1615476540/DemoVideos/ParentDemo2.mp4'
+          'https://res.cloudinary.com/haydentech/video/upload/v1615476540/DemoVideos/ParentDemo2.mov'
         }
       />
       {/* <AboutUl>
@@ -51,10 +51,10 @@ const AboutParent = ({ setView }) => {
           <li>Find studios, retailers, and opportunities for your dancers!</li>
         </AboutUl> */}
     </div>
-  )
-}
+  );
+};
 
-export default AboutParent
+export default AboutParent;
 //  <button
 //         type='button'
 //         className='btn-action-primary'
